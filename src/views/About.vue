13cc0d1aae37ea22b01f0b0181bf5367a791bffd
Paul R. De Buck <polichinel8@gmail.com>
2019-07-18 15:27:43 +0200

    new sceen

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <router-link to="/"><ion-button size="small" fill="outline">Go home</ion-button></router-link>
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,82 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="ion-page">
+    <ion-content no-padding>
+      <h1 text-center padding>
+        Supply Management System
+        <br />
+        <span color="medium">on the public Blockchain</span>
+      </h1>
+      <ion-grid>
+        <ion-row>
+          <ion-col>
+            <ion-card>
+              <ion-card-header>
+                <ion-card-subtitle>Fish arrived at port</ion-card-subtitle>
+                <ion-card-title>Fisheries</ion-card-title>
+              </ion-card-header>
+            </ion-card>
+          </ion-col>
+          <ion-col>
+            <ion-card>
+              <ion-card-header>
+                <ion-card-subtitle>Just arrived in China</ion-card-subtitle>
+                <ion-card-title>Metal material</ion-card-title>
+              </ion-card-header>
+            </ion-card>
+          </ion-col>
+        </ion-row>
+      </ion-grid>
+    </ion-content>
+    <ion-footer>
+      <ion-toolbar>
+        <ion-grid no-padding>
+          <ion-row>
+            <ion-col no-padding text-center>
+              <router-link to="/">
+                <ion-icon name="person" class="home"></ion-icon>
+              </router-link>
+            </ion-col>
+            <ion-col no-padding text-center>
+              <router-link to="/about">
+                <ion-icon name="albums" class="home"></ion-icon>
+              </router-link>
+            </ion-col>
+            <ion-col no-padding text-center>
+              <router-link to="/wallet">
+                <ion-icon name="wallet" class="home"></ion-icon>
+              </router-link>
+            </ion-col>
+          </ion-row>
+        </ion-grid>
+      </ion-toolbar>
+    </ion-footer>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "home"
+};
 </script>
+
+<style lang="scss">
+ion-content {
+  --background: url("../assets/background.jpeg") no-repeat center center / cover;
+}
+h1 {
+  color: $dark;
+}
+h1 span {
+  font-size: 0.7em;
+  font-style: italic;
+  color: $medium;
+}
+.home {
+  font-size: 2em;
+  height: 50px;
+  color: $primary;
+}
+ion-card {
+  background: linear-gradient(180deg, white, $light) !important;
+}
+</style>
